2aac5cdf685101e21915eb682c947bf45038d041
choiminseong <choiminseong@.com>
2023-01-02 22:25:56 +0900

    수업

---
+++ Day2/condition.py
@@ -0,0 +1,97 @@
+''' 조건문
+1. if/else  
+if 조건 :
+    참일때 실행할거 작성
+elif 또 다른 조건 :
+    조건은 거짓 또 다른 조건이 참일때 작성
+else :
+    거짓일때 실행할거 작성
+
+2-1. for
+for i in range(반복숫자):
+    실행할 내용
+
+* 0부터 시작
+
+2-2
+for 변수 in range (시작,끝):
+    실행할 내용
+
+* 시작은 이상,끝은 미만
+** 1씩 증가
+
+2-3
+for 변수 in range(시작,끝,증감):
+    실행할 내용
+
+java: for(var i=시작;i<끝;증감){
+    실행할 내용
+}
+
+
+'''
+# apple=10
+# orange=20
+# if apple>orange:
+#     print("사과가 큼")
+# else:
+#     print("오렌지가 큼")
+
+# a=5
+# b=5
+# if a>b:
+#     print("a가 큼")
+# else a<b:
+#     print("b가 큼")
+# else :
+#     print("같음")
+ 
+# for i in range(3):
+#     print("Hi")
+
+# for i in range(1,11):
+#     print(i)
+
+# for i in range(2,11,2):
+#     print(i)
+
+# for i in range(1,11,2):
+#     print(i)
+
+
+
+
+# for i in range(1,11):
+#     if i%2==1:
+#         print(i)
+
+# for i in range(1,11):
+#     if i%3!=0:
+#         print(i)
+
+# for i in range(50,100):
+#     if i%3==0 :
+#         if i%4==0 :
+#             print(i)
+
+# a=1
+# for i in range(1,20):
+#     if i%7==0:
+#         a*=i
+# print(a)
+        
+# a=0
+# for i in range(50,60):
+#     if i%4!=0:
+#         a+=i
+# print(a)
+
+# a=0
+# for i in range(1,101):
+#     if i%2==0:
+#         a+=i
+# b=0
+# for i in range(1,101):
+#     if i%2==1:
+#         b+=i
+# print(a-b)
